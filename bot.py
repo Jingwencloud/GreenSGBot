c02c5b9bf6ae79cb26e09524ad6349e65db3c6e6
Jingwencloud <jingwen098765432@gmail.com>
2022-07-10 00:01:38 +0800

    Update geolocator

--- bot.py
+++ bot.py
@@ -4,14 +4,11 @@ import telegram
 from telegram import Update
 from telegram.ext import Updater, Application, CommandHandler, ContextTypes, MessageHandler, filters
 import os
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 from query import search
 import geopy
-import asyncio
 
 
+geolocator = geopy.Nominatim(user_agent='recycleTeleBot972022')
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 logging.basicConfig(
@@ -42,3 +39,11 @@ async def manage_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for msg in messages:
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text = msg)
+
+if __name__ == '__main__':
+    start_handler = CommandHandler('start', start)
+    search_bin_handler = CommandHandler('search_bin', search_bin)
+    dp.add_handler(start_handler)
+    dp.add_handler(search_bin_handler)
+
+    dp.add_handler(MessageHandler(filters.LOCATION & ~filters.COMMAND, manage_location))
